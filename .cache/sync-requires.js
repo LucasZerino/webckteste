c204eca66a93cd36ee5209fbaf5c2691ca2f501e
Lucas Zerino <101360532+LucasZerino@users.noreply.github.com>
2022-04-01 02:45:18 -0300

    primeiro commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,12 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Lucas\\Desktop\\AQUI\\webck0104\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\Lucas\\Desktop\\AQUI\\webck0104\\src\\pages\\404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\Lucas\\Desktop\\AQUI\\webck0104\\src\\pages\\index.js")))
+}
+
